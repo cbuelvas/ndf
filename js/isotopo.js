d35834aa323eb2a02de0b7df86115e1c413802fb
carlos <cbuelvas@gmail.com>
2015-03-16 13:57:21 -0500

    primera subida

---
+++ js/isotopo.js
@@ -0,0 +1,12 @@
+$( function() {
+
+    $('.isotope').isotope({
+        itemSelector: '.item',
+        masonry: {
+            columnWidth: 100
+        }
+    });
+
+});/**
+ * Created by carlos on 16/03/15.
+ */
